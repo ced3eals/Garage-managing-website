c6f32d2216de308c09008dfa681c32ec2c946aa7
Glitch (lincey-esiea) <none>
2018-09-07 11:56:53 +0000

    ⏳☎️ Checkpoint ./server.js:1717361/902

--- server.js
+++ server.js
@@ -13,26 +13,26 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(sessions({
-  secret: 'test'
+  secret: 'fevrzdsvsdvé$$ù'
 }))
 app.use('/public', express.static('public'));
 // Parse mail and password
 var contenu;
-contenu = fs.readFileSync("./storage/account.json", "UTF-8");
+contenu = fs.readFileSync("./src/logs.json", "UTF-8");
 var js = JSON.parse(contenu);
 
-app.get('/login', function (req,resp){
+app.get('/sign_in', function (req,resp){
   session = req.session;
   if (session.uniqueID) {
     resp.redirect('/redirects');
   }
 
-  resp.sendFile(__dirname + '/views/signin.html');
-  //resp.sendFile('./public/contact.html',{root:__dirname});
+  resp.sendFile(__dirname + '/views/sign_in.html');
 
+  
 });
 //Sign up
-app.get('/signup', function(req,resp){
+app.get('/sign_in', function(req,resp){
   resp.sendFile('./src/signUp.html',{root:__dirname});
 })
 
@@ -40,7 +40,7 @@ app.get('/signup', function(req,resp){
 app.post('/login', function (req,resp){
   //resp.end(JSON.stringify(req.body));
   var contenu;
-  contenu = fs.readFileSync("./storage/account.json", "UTF-8");
+  contenu = fs.readFileSync("./src/logs.json", "UTF-8");
   var js = JSON.parse(contenu);
   session = req.session;
   if (session.uniqueID) {
@@ -84,32 +84,9 @@ app.post('/createAccount', function(req,resp){ //Post Response
     } catch (error) {
         console.log(error);
     }
-  resp.sendFile('./src/contact.html',{root:__dirname});
+  resp.sendFile('./src/create.html',{root:__dirname});
 });
 
-app.post('/', function(req,resp){ //Post Response
-  var familyName = req.body.familyName;
-  var name = req.body.name;
-  var promotion = req.body.promotion;
-  var mail = req.body.email;
-  var events = req.body.event;
-
-   try {
-        let userData = fs.readFileSync('./storage/attendants.json');
-        userData = JSON.parse(userData);
-        userData.push({
-           FamilyName:familyName,
-           Name:name,
-           promotion:promotion,
-           email:mail,
-           events:events
-        });
-        fs.writeFileSync('./storage/attendants.json', JSON.stringify(userData,null,2));
-    } catch (error) {
-        console.log(error);
-    }
-  resp.sendFile('./src/contact.html',{root:__dirname});
-});
 
 
 app.listen(3000,function(){

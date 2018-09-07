cf0009b03a572491fd3fe1933217edb50ba26115
Glitch (lincey-esiea) <none>
2018-09-07 09:30:18 +0000

    🍬🍥 Checkpoint ./server.js:1717361/1681 ./public/style.css:1717361/2 ./public/index.html:1717361/159

--- server.js
+++ server.js
@@ -1,43 +1,33 @@
+
+
 var express = require('express');
-var bodyParser = require('body-parser');
-var sessions = require('express-session');
-
-
 var app = express();
-var session;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.use(sessions({
-  secret: 'uinjmiomlnôpùçiyè_yoiulhnkj',
-  resave: false,
-  saveUninitialized: true
-}))
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile('public/sign_in.html', {root: __dirname});
+app.use('/', function (req, res, next) {
+
+  var options = {
+    root: __dirname + '/public/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name;
+  res.sendFile('./404.html', options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', './404.html');
+    }
+  });
+
 });
-
-app.post('login', function(req, res){
-  res.end(JSON.stringify(req.body));
-  if(req.body.email == 'admin' && req.body.password =='admin'){
-    session.id = req.body.username;
-  }
-  res.redirect('/redirects');
-});
-
-
-app.get('/redirects', function (req, resp){
-  if (session.id){
-    resp.redirect('admin');
-  } else {
-    resp.end('Who are you ?');
-  }
-});
-
 
 var server = app.listen(process.env.PORT || 5000, function () {
 

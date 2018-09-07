da30cf7107083eb057d58dca36ecf01069a3f706
Glitch (lincey-esiea) <none>
2018-09-07 11:46:45 +0000

    🌿🏬 Checkpoint ./server.js:1717361/3858

--- server.js
+++ server.js
@@ -1,38 +1,117 @@
-
+var http = require('http');
+var fs = require('fs');
 var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var vm = require('vm');
+var sessions = require('express-session');
+var jsonFile = require('jsonfile');
+
+var session;
+
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(sessions({
+  secret: 'test'
+}))
+app.use('/public', express.static('public'));
+// Parse mail and password
+var contenu;
+contenu = fs.readFileSync("./storage/account.json", "UTF-8");
+var js = JSON.parse(contenu);
 
-app.use(express.static('public'));
+app.get('/login', function (req,resp){
+  session = req.session;
+  if (session.uniqueID) {
+    resp.redirect('/redirects');
+  }
 
-app.use('/', function (req, res, next) {
+  resp.sendFile(__dirname + '/views/signin.html');
+  //resp.sendFile('./public/contact.html',{root:__dirname});
 
-  var options = {
-    root: __dirname + '/public/',
-    dotfiles: 'deny',
-    headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
-    }
-  };
+});
+//Sign up
+app.get('/signup', function(req,resp){
+  resp.sendFile('./src/signUp.html',{root:__dirname});
+})
 
-  var fileName = req.params.name;
-  res.sendFile('./404.html', options, function (err) {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    }
-    else {
-      console.log('Sent:', './404.html');
-    }
+//Login test
+app.post('/login', function (req,resp){
+  //resp.end(JSON.stringify(req.body));
+  var contenu;
+  contenu = fs.readFileSync("./storage/account.json", "UTF-8");
+  var js = JSON.parse(contenu);
+  session = req.session;
+  if (session.uniqueID) {
+    resp.redirect('/redirects');
+  }
+  for(var i = 0; i<js.length; i++){
+  if(req.body.email == js[i].email && req.body.password == js[i].password){
+    session.uniqueID = req.body.email;
+  }
+  }
+  resp.redirect('/redirects');
+});
+
+app.post('/logout', function(req,resp){
+  req.session.destroy(function(error){
+    console.log(error);
+    resp.redirect('/login');
   });
+});
+app.get('/redirects', function(req,resp){
+  session = req.session;
+  if (session.uniqueID) {
+    resp.sendFile('./src/contact.html',{root:__dirname});
+  }
+  else {
+    resp.sendFile(__dirname + '/views/signin.html');
+  }
+})
+app.post('/createAccount', function(req,resp){ //Post Response
+  var mail = req.body.email;
+  var password = req.body.password;
 
+   try {
+        let userData = fs.readFileSync('./storage/account.json');
+        userData = JSON.parse(userData);
+        userData.push({
+           email:mail,
+           password:password
+        });
+        fs.writeFileSync('./storage/account.json', JSON.stringify(userData,null,2));
+    } catch (error) {
+        console.log(error);
+    }
+  resp.sendFile('./src/contact.html',{root:__dirname});
 });
 
-var server = app.listen(process.env.PORT || 5000, function () {
+app.post('/', function(req,resp){ //Post Response
+  var familyName = req.body.familyName;
+  var name = req.body.name;
+  var promotion = req.body.promotion;
+  var mail = req.body.email;
+  var events = req.body.event;
 
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('I am listening at http://%s:%s', host, port);
-
+   try {
+        let userData = fs.readFileSync('./storage/attendants.json');
+        userData = JSON.parse(userData);
+        userData.push({
+           FamilyName:familyName,
+           Name:name,
+           promotion:promotion,
+           email:mail,
+           events:events
+        });
+        fs.writeFileSync('./storage/attendants.json', JSON.stringify(userData,null,2));
+    } catch (error) {
+        console.log(error);
+    }
+  resp.sendFile('./src/contact.html',{root:__dirname});
 });
+
+
+app.listen(3000,function(){
+  console.log('Listening at port 3000');
+})
